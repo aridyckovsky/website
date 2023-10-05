b3b974007366ad5f966c3a2c36e551bf49571971
Ari Dyckovsky <aridyckovsky@gmail.com>
2023-10-05 18:14:56 -0400

    increase font and remove borders of sidebar

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -11,16 +11,11 @@ const SideBar = () => (
       <Link to="/" className="logo">
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
-      <header>
-        <h2>Ari Dyckovsky</h2>
-        <p><a href="mailto:ari@aridyckovsky.com">ari@aridyckovsky.com</a></p>
-      </header>
     </section>
 
     <section className="blurb">
-      <h2>About</h2>
       <p>Hi, I&apos;m Ari. I&apos;m a PhD Candidate in <a href="https://psych.princeton.edu/">Psychology at Princeton University</a>, where
-        I study collective psychological processes in large-scale social networks with Dr. Alin Coman in the <a href="https://comanlab.com/">Collectives in Cognition Lab</a>.
+        I study collective psychological processes in large-scale social networks in the <a href="https://comanlab.com/">Cognition in Collectives Lab</a>.
       </p>
       <ul className="actions">
         <li>
